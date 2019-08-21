0b76124dbfd2279c593e3b15cad15d8639660461
Ben Eccles <ben.g.eccles@icloud.com>
2019-08-21 16:15:25 -0600

    added solution for typeahead through step 3

---
+++ vanilla-typeahead/index.js
@@ -0,0 +1,28 @@
+const characters = [];
+let list = document.getElementById('list');
+let search = document.querySelector('input');
+
+fetch('https://www.swapi.co/api/people')
+    .then(response => response.json())
+    .then(response => {
+        characters.push(...response.results);
+    })
+
+function filterText () {
+    // Convert all characters to lower case, and map into li tags.
+    const filtered = characters.filter(i => i.name.toLowerCase().includes
+    (this.value.toLowerCase()))
+    .map(i => `<li>${i.name}</li>`);
+
+    if (filtered.length){
+        render (filtered);
+    }
+}
+
+function render (arr) {
+    let html = arr.join('');
+    list.innerHTML = html;
+}
+
+
+search.addEventListener('keyup', filterText);
